93c540b7f2fa5cdc7023547c1f5023d9986a21ef
Michael Fry <mfry1199@gmail.com>
2025-12-28 21:40:51 -0600

    added endpoint

--- ftr_flags/api.py
+++ ftr_flags/api.py
@@ -11,6 +11,11 @@ def create_app(ftr_mgr: FeatureFlagManager) -> FastAPI:
     async def evaluate(request: dict):
         response = ftr_mgr.eval_ftr_flag(request)
         return response
+    
+    @app.post("/health")
+    async def health():
+        return 200
+
 
 
     return app
